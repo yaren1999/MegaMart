01496db610c144a0c419a6f2a77bc09b8a5f6811
yaren1999 <demirciyarenmisra@gmail.com>
2025-04-08 20:17:39 +0300

    product, ProductList ve data products.json eklendi

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const nextConfig = {
+    images: {
+        domains: [
+            "productimages.hepsiburada.net",
+            "images.hepsiburada.net"
+        ],
+
+    },
+}
+
+module.exports = nextConfig
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ProductList from "@/components/ProductList/ProductList";
+import products from "../data/products.json"
+
+
 
 export default function Home() {
   return (
-   <div></div>
+   <div>
+   
+   <ProductList productList={products} />
+
+   </div>
   );
 }
